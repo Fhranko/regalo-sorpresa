3dde7de36734a22831e9fb390c15ddc5c5a9fc8a
Fhranko <francohuanaco@gmail.com>
2020-08-12 17:00:13 -0400

    Se muestran los datos al administrador

--- src/app/components/panel/panel.component.ts
+++ src/app/components/panel/panel.component.ts
@@ -4,13 +4,20 @@ import { AngularFirestore } from '@angular/fire/firestore';
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
-  styleUrls: ['./panel.component.css']
+  styleUrls: ['./panel.component.css'],
 })
 export class PanelComponent implements OnInit {
-
-  constructor(    private angularFireStore: AngularFirestore) { }
+  registros = [];
+  constructor(private angularFireStore: AngularFirestore) {}
 
   ngOnInit(): void {
+    let valores = this.angularFireStore
+      .collection('registros')
+      .valueChanges()
+      .subscribe((res) => {
+        // console.log(res)
+        this.registros = res;
+        console.log(this.registros);
+      });
   }
-
 }
