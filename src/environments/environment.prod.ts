7c2057ef97c3ce09d04fc758cdb47438fccf35a0
Fhranko <francohuanaco@gmail.com>
2020-08-12 17:35:36 -0400

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyD-0GBo9ShP7CYfk2eS2zvgrcjOO2LcCUw",
+    authDomain: "caja-sorpresa-eb892.firebaseapp.com",
+    databaseURL: "https://caja-sorpresa-eb892.firebaseio.com",
+    projectId: "caja-sorpresa-eb892",
+    storageBucket: "caja-sorpresa-eb892.appspot.com",
+    messagingSenderId: "989631081954",
+    appId: "1:989631081954:web:b295372ec30084d8fbddbb"
+  }
 };
